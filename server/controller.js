06f9ce65b193d2b840adc8a157709833a0030825
Benjamin Salmon <bensalmon8@gmail.com>
2022-07-22 11:09:05 -0400

    initial commit

---
+++ server/controller.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres', 
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
+// server talking to database
+module.exports = {
+    getInputs: (req, res) => {
+        sequelize
+            .query(`SELECT * FROM input;`)
+            .then((dbRes) => res.status(200).send(dbRes[0]))
+            .catch((err) => console.log('GETTING', err));
+    },
+    createInput: (req, res) => {
+        const { text } = req.body;
+
+        sequelize
+            .query(`INSERT INTO input (text) VALUES ('${text}');`)
+            .then((dbRes) => res.status(200).send(dbRes[0]))
+            .catch((err) => console.log('POSTING', err));
+    }
+}
